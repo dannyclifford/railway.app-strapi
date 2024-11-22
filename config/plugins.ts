b09d042e20de805061462c22ab0950607e2cda57
Danny <dcliff10@gmail.com>
2024-11-22 15:04:59 -0600

    Email provider

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,22 +1,35 @@
 export default ({ env }) => ({
-    'users-permissions': {
-        config: {
-            jwtSecret: env('JWT_SECRET'),
-        },
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET"),
     },
-    upload: {
-        config: {
-            provider: 'cloudinary',
-            providerOptions: {
-                cloud_name: env('CLOUDINARY_NAME'),
-                api_key: env('CLOUDINARY_KEY'),
-                api_secret: env('CLOUDINARY_SECRET'),
-            },
-            actionOptions: {
-                upload: {},
-                uploadStream: {},
-                delete: {},
-            },
-        },
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
     },
+  },
+  email: {
+    config: {
+      provider: "@strapi/provider-email-sendgrid", // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: "daniel@hermigranthub.org",
+        defaultReplyTo: "daniel@hermigranthub.org",
+        testAddress: "dannyclifford10@gmail.com",
+      },
+    },
+  },
 });
