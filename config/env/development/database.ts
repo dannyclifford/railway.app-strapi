7e67585d3acadb17f5f3d8a4fa6068d27a053505
Danny <dcliff10@gmail.com>
2024-11-22 14:21:44 -0600

    Remove sqlite causing issues

--- config/env/development/database.ts
+++ config/env/development/database.ts
@@ -1,19 +1,19 @@
-// export default ({ env }) => ({
-//     connection: {
-//         client: 'postgres',
-//         connection: {
-//             connectionString: env('DATABASE_URL')
-//         },
-//         acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
-//     }
-// });
-
 export default ({ env }) => ({
-  connection: {
-    client: "sqlite",
     connection: {
-      filename: ".tmp/data.db",
-    },
-    useNullAsDefault: true,
-  },
+        client: 'postgres',
+        connection: {
+            connectionString: env('DATABASE_URL')
+        },
+        acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+    }
 });
+
+// export default ({ env }) => ({
+//   connection: {
+//     client: "sqlite",
+//     connection: {
+//       filename: ".tmp/data.db",
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
