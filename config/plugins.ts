4f5a077b4ad63fa9072c3419c1ceb8393209e3d4
Danny <dcliff10@gmail.com>
2024-11-22 15:30:31 -0600

    Changed sendgrid provider name

--- config/plugins.ts
+++ config/plugins.ts
@@ -21,7 +21,7 @@ export default ({ env }) => ({
   },
   email: {
     config: {
-      provider: "@strapi/provider-email-sendgrid", // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
+      provider: "sendgrid", // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
       providerOptions: {
         apiKey: env("SENDGRID_API_KEY"),
       },
